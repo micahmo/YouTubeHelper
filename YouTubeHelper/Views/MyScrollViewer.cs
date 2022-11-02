79670cccf55999219bd456fc7d4bbb7ea642574f
Micah Morrison <micahmorrison95@gmail.com>
2022-11-02 10:08:52 -0400

    Allow PageUp/Down to propagate up from ScrollViewer

---
+++ YouTubeHelper/Views/MyScrollViewer.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace YouTubeHelper.Views
+{
+    public class MyScrollViewer : ScrollViewer
+    {
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            if (e.Key is Key.PageUp or Key.PageDown)
+            {
+                return;
+            }
+
+            base.OnKeyDown(e);
+        }
+    }
+}
