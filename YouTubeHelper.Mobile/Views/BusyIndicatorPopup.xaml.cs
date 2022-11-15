7ee0bafda1228b2becfeb113b99210d8be44302d
Micah Morrison <micahmorrison95@gmail.com>
2022-11-15 09:56:11 -0500

    Fix issue with dismissable busy indicator

--- YouTubeHelper.Mobile/Views/BusyIndicatorPopup.xaml.cs
+++ YouTubeHelper.Mobile/Views/BusyIndicatorPopup.xaml.cs
@@ -7,6 +7,7 @@ namespace YouTubeHelper.Mobile.Views
         public BusyIndicatorPopup()
         {
             InitializeComponent();
+            CanBeDismissedByTappingOutsideOfPopup = false;
         }
     }
 
