26e9d66799b43e48b7491486ba5996b0db03d859
Micah Morrison <micahmorrison95@gmail.com>
2024-09-07 17:23:28 -0400

    Fix colors in app

--- YouTubeHelper.Mobile/Platforms/Android/MainActivity.cs
+++ YouTubeHelper.Mobile/Platforms/Android/MainActivity.cs
@@ -1,11 +1,22 @@
 ﻿using Android.App;
 using Android.Content.PM;
-using Microsoft.Maui;
+using Android.OS;
+using Color = Android.Graphics.Color;
 
 namespace YouTubeHelper.Mobile
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                // Set the nav bar color
+                Window?.SetNavigationBarColor(Color.Firebrick);
+            }
+        }
     }
 }
