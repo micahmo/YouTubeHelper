99a33879d40d022b7c5a08a9ca88047821a43dc8
Micah Morrison <micahmorrison95@gmail.com>
2025-04-16 11:33:35 -0400

    Reconnect SignalR after server address changes (desktop)

--- YouTubeHelper/Views/SettingsControl.xaml.cs
+++ YouTubeHelper/Views/SettingsControl.xaml.cs
@@ -1,4 +1,12 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using MongoDBHelpers;
+using ServerStatusBot.Definitions.Api;
+using ServerStatusBot.Definitions.Database.Models;
+using YouTubeHelper.Utilities;
 
 namespace YouTubeHelper.Views
 {
@@ -7,9 +15,52 @@ namespace YouTubeHelper.Views
     /// </summary>
     public partial class SettingsControl : UserControl
     {
+        private static bool _registeredForSettingsChanges;
+        
         public SettingsControl()
         {
             InitializeComponent();
+
+            if (!_registeredForSettingsChanges)
+            {
+                _registeredForSettingsChanges = true;
+                Settings.Instance.PropertyChanged += Settings_PropertyChanged;
+            }
+        }
+
+        private static async void Settings_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            Exception? finalEx = null;
+
+            if (e.PropertyName == nameof(Settings.Instance.ServerAddress))
+            {
+                for (int i = 0; i < 10; ++i)
+                {
+                    if (!string.IsNullOrEmpty(DatabaseEngine.ConnectionString))
+                    {
+                        try
+                        {
+                            await ServerApiClient.Instance.ReconnectAllGroups();
+                            break;
+                        }
+                        catch (Exception ex)
+                        {
+                            finalEx = ex;
+                        }
+
+                        await Task.Delay(TimeSpan.FromSeconds(1));
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if (finalEx != null)
+            {
+                Application.Current?.Dispatcher.Invoke(async () => await MessageBoxHelper.ShowCopyableText(Properties.Resources.UnexpectedError, Properties.Resources.Error, finalEx.ToString()));
+            }
         }
     }
 }
