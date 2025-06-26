e6fbe649d1125a0e2fd669e1cd10826b8fc57a72
Micah Morrison <micahmorrison95@gmail.com>
2025-06-26 14:13:27 -0400

    Put title back where it belongs

--- YouTubeHelper/MainWindow.xaml.cs
+++ YouTubeHelper/MainWindow.xaml.cs
@@ -48,6 +48,8 @@ namespace YouTubeHelper
             InitializeComponent();
             Instance = this;
 
+            Title = $"{Properties.Resources.ApplicationName} {Versioning.GetInstalledMsiVersion()}";
+
             // Check for updates
             _updateChecker = new MyUpdateChecker("https://gist.githubusercontent.com/micahmo/2f8966f2a9acbc8d11d70d69dc75c34c/raw/YouTubeHelperVersionInfo.xml", this);
         }
@@ -61,8 +63,6 @@ namespace YouTubeHelper
 
         private async void NavigationView_Loaded(object sender, RoutedEventArgs e)
         {
-            Title = $"{Properties.Resources.ApplicationName} {Versioning.GetInstalledMsiVersion()}";
-
             await ConnectToServer();
 
             // Connect to queue updates over SignalR
