386befbdf8c93ad52f6a73e4efc1113a4941cfa4
Micah Morrison <micahmorrison95@gmail.com>
2025-07-10 11:04:54 -0400

    Tweak UI for filter options

--- YouTubeHelper.Mobile/Views/FilterOptionsPopup.xaml.cs
+++ YouTubeHelper.Mobile/Views/FilterOptionsPopup.xaml.cs
@@ -10,6 +10,12 @@ namespace YouTubeHelper.Mobile.Views
             InitializeComponent();
 
             BindingContext = vm;
+
+            // Set width as a percentage of the device width
+            DisplayInfo displayInfo = DeviceDisplay.MainDisplayInfo;
+            double percent = 0.85;
+            double width = displayInfo.Width / displayInfo.Density * percent;
+            Size = new Size(width, -1);
         }
     }
 }
