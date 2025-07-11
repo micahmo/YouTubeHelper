7f2acff584a5820c9594fda188bba4c311f3f03c
Micah Morrison <micahmorrison95@gmail.com>
2025-07-10 23:24:23 -0400

    Remove filter options popup width calculation

--- YouTubeHelper.Mobile/Views/FilterOptionsPopup.xaml.cs
+++ YouTubeHelper.Mobile/Views/FilterOptionsPopup.xaml.cs
@@ -11,11 +11,11 @@ namespace YouTubeHelper.Mobile.Views
 
             BindingContext = vm;
 
-            // Set width as a percentage of the device width
-            DisplayInfo displayInfo = DeviceDisplay.MainDisplayInfo;
-            double percent = 0.85;
-            double width = displayInfo.Width / displayInfo.Density * percent;
-            Size = new Size(width, -1);
+            //// Set width as a percentage of the device width
+            //DisplayInfo displayInfo = DeviceDisplay.MainDisplayInfo;
+            //double percent = 0.85;
+            //double width = displayInfo.Width / displayInfo.Density * percent;
+            //Size = new Size(width, -1);
         }
     }
 }
