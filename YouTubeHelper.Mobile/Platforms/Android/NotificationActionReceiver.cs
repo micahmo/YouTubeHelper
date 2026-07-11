8cb6f712690293a7cdc64c4786bac29cbd8b0c65
Micah Morrison <micahmorrison95@gmail.com>
2026-07-11 17:13:56 -0400

    Fix Obtainium deep link URI format in notification action receiver

--- YouTubeHelper.Mobile/Platforms/Android/NotificationActionReceiver.cs
+++ YouTubeHelper.Mobile/Platforms/Android/NotificationActionReceiver.cs
@@ -34,7 +34,7 @@ namespace YouTubeHelper.Mobile.Platforms.Android
                 case "openObtainium":
                     AndroidUtils.DismissNotification(context, notificationId);
                     Intent obtainiumIntent = new(Intent.ActionView);
-                    obtainiumIntent.SetData(global::Android.Net.Uri.Parse("obtainium://add/https://github.com/micahmo/YouTubeHelper"));
+                    obtainiumIntent.SetData(global::Android.Net.Uri.Parse("obtainium://add?url=https://github.com/micahmo/YouTubeHelper"));
                     obtainiumIntent.SetFlags(ActivityFlags.NewTask);
                     context.StartActivity(obtainiumIntent);
                     break;
